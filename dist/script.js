c478553553a73acbe9574e2d9afc0df2c63f6a2e
ZHADOW999 <143715858+ZHADOW999@users.noreply.github.com>
2024-06-28 17:32:17 +0100

    Delete dist directory

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".nav-list");
-
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active2");
-    offScreenMenu.classList.toggle("active2");
-    
-});
